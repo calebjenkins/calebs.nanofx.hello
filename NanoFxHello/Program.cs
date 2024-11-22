4c7e3e2e3fbefd648b547a73d71d68fcbeaeae39
calebjenkins <caleb@calebjenkins.com>
2024-11-21 23:43:31 -0600

    initial hackyness.. LED light is flashing - yay!

---
+++ NanoFxHello/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Device.Gpio;
+
+namespace NanoFxHello
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            LEDon();
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+
+        private static GpioController _gpio;
+        private static void LEDon()
+        {
+            _gpio = new GpioController();
+            GpioPin led = _gpio.OpenPin(2, PinMode.Output);
+            led.Write(PinValue.Low);
+            while (true)
+            {
+                // One
+                led.Write(PinValue.High);
+                Thread.Sleep(1000);
+                led.Write(PinValue.Low);
+                Thread.Sleep(1000);
+
+                // Two
+                led.Write(PinValue.High);
+                Thread.Sleep(1000);
+                led.Write(PinValue.Low);
+                Thread.Sleep(1000);
+
+                // Three
+                led.Write(PinValue.High);
+                Thread.Sleep(1000);
+                led.Write(PinValue.Low);
+                Thread.Sleep(3000);
+            }   
+        }
+    }
+}
---
+++ NanoHelloTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using nanoFramework;
+using System;
+
+namespace NanoHelloTests
+{
+    //[TestClass]
+    //public class Test1
+    //{
+    //    [TestMethod]
+    //    public void TestMethod1()
+    //    {
+    //    }
+    //}
+}
